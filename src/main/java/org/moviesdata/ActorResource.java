522bf27c49e6f770db62bd7eecda2016d81ce4b0
leropo00 <leropo00@gmail.com>
2023-09-09 11:40:12 +0200

    added counts on existing endpoints, tested via localhost:8080/q/metrics/application

--- src/main/java/org/moviesdata/ActorResource.java
+++ src/main/java/org/moviesdata/ActorResource.java
@@ -6,6 +6,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.metrics.annotation.Counted;
 
 @Path("/actors")
 @Produces(MediaType.APPLICATION_JSON)
@@ -13,6 +14,7 @@ import jakarta.ws.rs.core.Response;
 public class ActorResource {
 
     @GET
+    @Counted(name = "getAllActors", description = "count for: /actors")
     public Response getAllActors() {
         return Response.ok().build();
     }
--- src/main/java/org/moviesdata/MovieResource.java
+++ src/main/java/org/moviesdata/MovieResource.java
@@ -6,6 +6,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.metrics.annotation.Counted;
 
 @Path("/movies")
 @Produces(MediaType.APPLICATION_JSON)
@@ -13,6 +14,7 @@ import jakarta.ws.rs.core.Response;
 public class MovieResource {
 
     @GET
+    @Counted(name = "getAllMovies", description = "count for: /movies")
     public Response getAllActors() {
         return Response.ok().build();
     }
