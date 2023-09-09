2bbe16a649a202acd528463841c1454579e6f548
leropo00 <leropo00@gmail.com>
2023-09-09 13:32:31 +0200

    added endpoint for get actor by id

--- src/main/java/org/moviesdata/controller/ActorResource.java
+++ src/main/java/org/moviesdata/controller/ActorResource.java
@@ -1,15 +1,16 @@
 package org.moviesdata.controller;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.moviesdata.domain.Actor;
+import org.moviesdata.domain.Movie;
 import org.moviesdata.service.ActorService;
 import org.moviesdata.service.MovieService;
+
+import java.util.Optional;
 
 @Path("/actors")
 @Produces(MediaType.APPLICATION_JSON)
@@ -24,4 +25,14 @@ public class ActorResource {
     public Response getAllActors() {
         return Response.ok(actorService.listAllActors()).build();
     }
+
+    @GET
+    @Path("/{id}")
+    @Counted(name = "getSingleActors", description = "count for: /actors/{id}")
+    public Response getAllActors(@PathParam("id") String actorId) {
+        Optional<Actor> actor = actorService.findActorById(actorId);
+        if(actor.isEmpty()) return Response.status(Response.Status.NOT_FOUND).build();
+        return Response.ok(actor.get()).build();
+    }
+
 }
