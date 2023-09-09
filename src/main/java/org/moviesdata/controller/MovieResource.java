006d021f29cef0f1653689ec7554b4d8afd9bbee
leropo00 <leropo00@gmail.com>
2023-09-09 12:25:55 +0200

    added working listing of all movies

--- src/main/java/org/moviesdata/controller/MovieResource.java
+++ src/main/java/org/moviesdata/controller/MovieResource.java
@@ -1,5 +1,6 @@
 package org.moviesdata.controller;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -7,15 +8,20 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.moviesdata.service.MovieService;
 
 @Path("/movies")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class MovieResource {
 
+    @Inject
+    MovieService movieService;
+
     @GET
     @Counted(name = "getAllMovies", description = "count for: /movies")
     public Response getAllActors() {
-        return Response.ok().build();
+
+        return Response.ok(movieService.listAllMovies()).build();
     }
 }
--- src/main/java/org/moviesdata/service/MovieService.java
+++ src/main/java/org/moviesdata/service/MovieService.java
@@ -1,7 +1,21 @@
 package org.moviesdata.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.moviesdata.model.MovieEntity;
+import org.moviesdata.repository.MovieRepository;
+
+import java.util.List;
 
 @ApplicationScoped
 public class MovieService {
+
+    @Inject
+    MovieRepository movieRepository;
+
+    public List<MovieEntity> listAllMovies() {
+
+       return movieRepository.findAll()
+                .list();
+    }
 }
