9a9e5428628df46a886f7474a30f2cf0a2db3b55
leropo00 <leropo00@gmail.com>
2023-09-09 12:25:06 +0200

    added movie columns to movie entity, added repository for CRUD operations

--- src/main/java/org/moviesdata/model/MovieEntity.java
+++ src/main/java/org/moviesdata/model/MovieEntity.java
@@ -1,10 +1,9 @@
 package org.moviesdata.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.Date;
 
 @Entity(name = "Movie")
 @Table(name = "movies")
@@ -14,4 +13,21 @@ public class MovieEntity {
     @Id
     @Column(name = "imdbID")
     private String imdbID;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "release_year")
+    private Integer releaseYear;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate;
 }
