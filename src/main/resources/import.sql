70b18e96dcdb3413b6bffc7d701d0bd222bb8c6a
leropo00 <leropo00@gmail.com>
2023-09-09 11:54:39 +0200

    added and tested the initial version of the data model

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,33 @@
+DROP TABLE  IF EXISTS  movies_cast;
+DROP TABLE  IF EXISTS  actors;
 DROP TABLE  IF EXISTS  movies;
 
 CREATE TABLE movies
 (
     imdbID       VARCHAR(20) NOT NULL PRIMARY KEY,
+    title        VARCHAR(255) NOT NULL,
+    description  VARCHAR(2000) NOT NULL,
+    release_year INTEGER,
     created_at   TIMESTAMP NOT NULL,
     updated_at   TIMESTAMP NOT NULL
 );
 
+CREATE TABLE actors
+(
+    imdbID      VARCHAR(20)  NOT NULL PRIMARY KEY,
+    first_name  VARCHAR(255) NOT NULL,
+    last_name   VARCHAR(255) NOT NULL,
+    gender      ENUM('male', 'female') NOT NULL,
+    birth_date  DATE,
+    created_at  TIMESTAMP NOT NULL,
+    updated_at  TIMESTAMP NOT NULL
+);
 
-INSERT INTO movies(imdbID, created_at, updated_at)
- VALUES('tt5027774', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+CREATE TABLE movies_cast
+(
+    movie_id VARCHAR(20) NOT NULL,
+    actor_id VARCHAR(20) NOT NULL,
+    PRIMARY KEY(movie_id, actor_id),
+    FOREIGN KEY (movie_id) REFERENCES movies(imdbID) ,
+    FOREIGN KEY (actor_id) REFERENCES actors(imdbID)
+);
