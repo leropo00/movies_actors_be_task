d51125e2469b58f89bd5a63447742f50ebf77c95
leropo00 <leropo00@gmail.com>
2023-09-09 11:56:15 +0200

    added sample data for current database model

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -31,3 +31,29 @@ CREATE TABLE movies_cast
     FOREIGN KEY (movie_id) REFERENCES movies(imdbID) ,
     FOREIGN KEY (actor_id) REFERENCES actors(imdbID)
 );
+
+ -- test data is inserted in separate queries for easier debuging, if queries fail --
+
+INSERT INTO movies(imdbID, title, description, release_year, created_at, updated_at)
+ VALUES('tt5027774','Three Billboards Outside Ebbing, Missouri',
+'A mother personally challenges the local authorities to solve her daughters murder when they fail to catch the culprit.',
+ 2017 , CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+
+INSERT INTO actors(imdbID, first_name, last_name, gender, birth_date, created_at, updated_at)
+VALUES('nm0000531','Frances', 'McDormand', 'female', '1957-06-23' , CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO actors(imdbID, first_name, last_name, gender, birth_date, created_at, updated_at)
+VALUES('nm0005377','Sam', 'Rockwell', 'male', '1968-11-05' , CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO actors(imdbID, first_name, last_name, gender, birth_date, created_at, updated_at)
+VALUES('nm0000437','Woody', 'Harrelson', 'male', '1961-07-23' , CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO movies_cast(movie_id, actor_id)
+VALUES ('tt5027774', 'nm0000531');
+
+INSERT INTO movies_cast(movie_id, actor_id)
+VALUES ('tt5027774', 'nm0005377');
+
+INSERT INTO movies_cast(movie_id, actor_id)
+VALUES ('tt5027774', 'nm0000437');
