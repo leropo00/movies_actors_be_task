9693e3cc7686064c069f93f9da9fdbc64f66a671
leropo00 <leropo00@gmail.com>
2023-09-11 22:15:44 +0200

    refactored tests and added checks of validity of output data

--- src/test/java/org/moviesdata/MovieResourceTest.java
+++ src/test/java/org/moviesdata/MovieResourceTest.java
@@ -4,21 +4,23 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import org.moviesdata.domain.Actor;
+import org.moviesdata.domain.Movie;
 
 @QuarkusTest
 public class MovieResourceTest {
 
     @Test
-    public void testMoviesEndpoint() {
+    public void testAllMoviesEndpoint() {
         given()
-          .when().get("/movies")
-          .then()
-             .statusCode(200);
+                .when().get("/movies")
+                .then()
+                .statusCode(200);
     }
 
     @Test
-    public void testActorsEndpoint() {
+    public void testAllActorsEndpoint() {
         given()
                 .when().get("/actors")
                 .then()
@@ -27,25 +29,46 @@ public class MovieResourceTest {
 
     @Test
     public void testGetMovieEndpoint() {
-        given()
-                .when().get("/movies/tt5027774")
-                .then()
-                .statusCode(200);
+        Movie movie =
+                given()
+                        .when().get("/movies/tt5027774")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(Movie.class);
 
-        given()
-                .when().get("/movies/tt0180093")
-                .then()
-                .statusCode(404);
+        assertEquals("tt5027774", movie.getImdbID());
 
     }
 
     @Test
     public void testGetActorEndpoint() {
-        given()
-                .when().get("/actors/nm0000437")
-                .then()
-                .statusCode(200);
+        Actor actor =
+                given()
+                        .when().get("/actors/nm0000531")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(Actor.class);
 
+        assertEquals("nm0000531", actor.getImdbID());
+
+    }
+
+
+    @Test
+    public void testNonExistingMovie() {
+
+        given()
+                .when().get("/movies/tt0180093")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void testNonExistingActor() {
         given()
                 .when().get("/actors/nm0001467")
                 .then()
