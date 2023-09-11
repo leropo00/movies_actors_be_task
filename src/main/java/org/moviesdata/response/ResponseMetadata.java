54f81bd7e91e045ffcb54c58de58f61503c00e84
leropo00 <leropo00@gmail.com>
2023-09-11 15:26:40 +0200

    added fields that will be displayed in output with pagination, added constructor for paginated output

--- src/main/java/org/moviesdata/response/ResponseMetadata.java
+++ src/main/java/org/moviesdata/response/ResponseMetadata.java
@@ -1,11 +1,12 @@
 package org.moviesdata.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.quarkus.panache.common.Page;
 import lombok.Data;
 
 @Data
 public class ResponseMetadata {
-
     Integer total;
 
     @JsonProperty("results_count")
@@ -14,9 +15,51 @@ public class ResponseMetadata {
     @JsonProperty("has_pagination")
     Boolean hasPagination;
 
-    public ResponseMetadata(int total)  {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("current_page_index")
+    // page indexing starts with 0
+    Integer currentPageIndex;
+
+    @JsonProperty("page_size")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    Integer pageSize;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("has_previous_page")
+    Boolean hasPreviousPage;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("previous_page_index")
+    Integer previousPageIndex;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("has_next_page")
+    Boolean hasNextPage;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("next_page_index")
+    Integer nextPageIndex;
+
+    public ResponseMetadata(int total) {
         this.total = total;
         this.resultCount = total;
         this.hasPagination = false;
+    }
+
+    public ResponseMetadata(int total, int resultCount, Page page) {
+        this.total = total;
+        this.resultCount = resultCount;
+        this.hasPagination = true;
+
+        this.currentPageIndex = page.index;
+        this.pageSize = page.size;
+        this.hasPreviousPage = this.currentPageIndex > 0;
+        if(this.hasPreviousPage) {
+            this.previousPageIndex = this.currentPageIndex - 1;
+        }
+        this.hasNextPage = true;
+        if(this.hasPreviousPage) {
+            this.nextPageIndex = this.currentPageIndex + 1;
+        }
     }
 }
