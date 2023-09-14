743b42dd698f8f1e4510771d88a7aaecb6cececb
leropo00 <leropo00@gmail.com>
2023-09-14 16:22:52 +0200

    EntityError can now return either single id or list of ids

--- src/main/java/org/moviesdata/response/errors/EntityError.java
+++ src/main/java/org/moviesdata/response/errors/EntityError.java
@@ -1,12 +1,20 @@
 package org.moviesdata.response.errors;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.moviesdata.constants.ErrorResponseCode;
+
+import java.util.List;
 
 public class EntityError extends ErrorResponse {
 
     @JsonProperty("entity_id")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String entityId;
+
+    @JsonProperty("entity_ids")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<String> entityIds;
 
     public EntityError() {}
     public EntityError(ErrorResponseCode code, String description) {
@@ -18,6 +26,12 @@ public class EntityError extends ErrorResponse {
         this.entityId = entityId;
     }
 
+    public EntityError(ErrorResponseCode code, String description, List<String> entityIds) {
+        super(code, description);
+        this.entityIds = entityIds;
+    }
+
+
     public String getEntityId() {
         return entityId;
     }
@@ -25,4 +39,13 @@ public class EntityError extends ErrorResponse {
     public void setEntityId(String entityId) {
         this.entityId = entityId;
     }
+
+    public List<String> getEntityIds() {
+        return entityIds;
+    }
+
+    public void setEntityIds(List<String> entityIds) {
+        this.entityIds = entityIds;
+    }
+
 }
